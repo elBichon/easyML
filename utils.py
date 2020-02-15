f4939a2bf8a7c4681fd6e172cf0ea8f11f55dd2b
elbichon <reuglewiczjeanedouard@gmail.com>
2020-02-15 18:47:10 +0100

    updating library with log transformation function

--- utils.py
+++ utils.py
@@ -18,11 +18,18 @@ from tpot import TPOTClassifier
 import re
 import spacy
 from sklearn.preprocessing import StandardScaler
-
+import math
 
 
 def read_dataset(name,separator):
+    ''' This function enables to read a given csv dataset
+        any kind of separator can be specified
+    '''
     data = pd.read_csv(name, sep=separator)
+    return data
+
+def convert_to_log(data, min_column):
+    data = math.log(data+min_column+1)
     return data
 
 def create_list_features(list_features,nb_components):
@@ -31,6 +38,7 @@ def create_list_features(list_features,nb_components):
         list_features.append('PC'+str(i+1))
         i += 1 
     return(list_features)
+
 
 def broad_analysis(data):
     print('shape of the dataset')
@@ -79,24 +87,26 @@ def broad_analysis(data):
         print('feature '+features_list[i])
         df[features_list[i]].value_counts(dropna=False)
         i += 1
-   # plt.boxplot(data)
-   # plt.ylim(0,10)
+    scatter_matrix(df)
+    plt.show()
     
+
 def scatterplotting(data):
     pd.plotting.scatter_matrix(data, alpha = 0.3, figsize = (40,40), diagonal = 'kde');
     
 def visualise_correlation(data):
     correlation = data.corr()
-    # display(correlation)
     plt.figure(figsize=(14, 12))
     heatmap = sns.heatmap(correlation, annot=True, linewidths=0, vmin=-1, cmap="RdBu_r")
     
+
 def visualise_specific_correlation(x_axis,y_axis):
     specific_data = df[[str(y_axis), str(x_axis)]]
     gridA = sns.JointGrid(x=x_axis, y=y_axis, data=specific_data, size=6)
     gridA = gridA.plot_joint(sns.regplot, scatter_kws={"s": 10})
     gridA = gridA.plot_marginals(sns.distplot)
     
+
 def categorical_correlation(category,y_axis):
     fig, axs = plt.subplots(ncols=1,figsize=(10,6))
     sns.barplot(x=str(category), y=str(y_axis), data=volatileAcidity_quality, ax=axs)
@@ -106,80 +116,59 @@ def categorical_correlation(category,y_axis):
     plt.show()
     plt.gcf().clear()
     
+
 def outliers_removal(data):
-        # For each feature find the data points with extreme high or low values
     for feature in data.keys():
         if data[feature].dtype == 'float64':
-        # TODO: Calculate Q1 (25th percentile of the data) for the given feature
             Q1 = np.percentile(data[feature], q=25)
-         # TODO: Calculate Q3 (75th percentile of the data) for the given feature
             Q3 = np.percentile(data[feature], q=75)
-        # TODO: Use the interquartile range to calculate an outlier step (1.5 times the interquartile range)
             interquartile_range = Q3 - Q1
             step = 1.5 * interquartile_range
-        # Display the outliers
             print("Data points considered outliers for the feature '{}':".format(feature))
             display(data[~((data[feature] >= Q1 - step) & (data[feature] <= Q3 + step))])
-    # OPTIONAL: Select the indices for data points you wish to remove
             outliers = []
-    # Remove the outliers, if any were specified
             good_data = data.drop(data.index[outliers]).reset_index(drop = True)
         else:
             pass
     return good_data
 
-# Function to calculate missing values by column# Funct 
+
 def missing_values_table(df):
-        # Total missing values
         mis_val = df.isnull().sum()
-        # Percentage of missing values
         mis_val_percent = 100 * df.isnull().sum() / len(df)
-        
-        # Make a table with the results
         mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
-        
-        # Rename the columns
         mis_val_table_ren_columns = mis_val_table.rename(
         columns = {0 : 'Missing Values', 1 : '% of Total Values'})
-        
-        # Sort the table by percentage of missing descending
         mis_val_table_ren_columns = mis_val_table_ren_columns[
             mis_val_table_ren_columns.iloc[:,1] != 0].sort_values(
         '% of Total Values', ascending=False).round(1)
-        
-        # Print some summary information
+
         print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"      
             "There are " + str(mis_val_table_ren_columns.shape[0]) +
               " columns that have missing values.")
-        
-        # Return the dataframe with missing information
         return mis_val_table_ren_columns
     
+
 def read_dataset(name,separator):
     data = pd.read_csv(name, sep=separator)
     return data
 
+
 def one_hot_encoder(df):
     le = LabelEncoder()
     le_count = 0
-    # Iterate through the columns
     for col in df:
         if df[col].dtype == 'object':
-            # If 2 or fewer unique categories
             if len(list(df[col].unique())) <= 2:
-                # Train on the training data
                 le.fit(df[col])
-                # Transform both training and testing data
                 df[col] = le.transform(df[col])            
-                # Keep track of how many columns were label encoded
                 le_count += 1
     print('%d columns were label encoded.' % le_count)
-    # one-hot encoding of categorical variables
     df = pd.get_dummies(df)
     print('Training Features shape: ', df.shape)
     return(df)
 
-#def remove_colinear_data():
+
 def remove_unique_feature(df):
     i = 0
     features_list = df.columns
@@ -189,8 +178,7 @@ def remove_unique_feature(df):
             df.drop(features_list[i], 1, inplace=True)
         i += 1
     return df
-#def eliminate_low_importance():
-#def perform_PCA()
+
     
 def missing_values(df,policy):
     i = 0
@@ -211,41 +199,6 @@ def missing_values(df,policy):
     print('treatment is over')
     return df
 
-def features_importance(data,information_import,target):
-    data_np = data.astype(np.int32).values
-    X = data_np[:,:-1]
-    y = data_np[:,-1]
-    model = ExtraTreesClassifier()
-    model.fit(X,y)
-# display the relative importance of each attribute
-    sorted_features = np.sort(model.feature_importances_)[::-1].tolist()
-    sum_info = 0
-    i = 0
-    while i < len(sorted_features):
-        if sum_info < information_import:
-            sum_info = sum_info+sorted_features[i]
-        elif sum_info > information_import:
-            break
-        i += 1
-    model = LogisticRegression()
-# create the RFE model and select 3 attributes
-    rfe = RFE(model, i)
-    rfe = rfe.fit(X,y)
-    # summarize the selection of the attributes
-    rank = rfe.ranking_.tolist()
-    i = 0
-    features_index = []
-    while i < len(rank):
-        if rank[i] == 1:
-            features_index.append(i)
-        i += 1
-    best_features = []
-    for index in features_index:
-        best_features.append(data.columns[index])
-    best_features.append(target)
-    print('most significant features', best_features)
-    return best_features
-
 
 def broad_analysis(data):
     print('shape of the dataset: ',data.shape)
@@ -260,6 +213,7 @@ def broad_analysis(data):
     print(data.isnull().any())
     print('generic infos about the dataset')
     print(data.info())
+
 
 def tendancy_indicators(dataframe,feature):
     print('average value mean = ',dataframe[feature].mean())
@@ -282,6 +236,7 @@ def tendancy_indicators(dataframe,feature):
     else:
         print('values are not concentrated')
     
+
 def data_relation(data,size):
     pd.plotting.scatter_matrix(data, alpha = 0.3, figsize = size, diagonal = 'kde')
     plt.show()
@@ -289,16 +244,13 @@ def data_relation(data,size):
     plt.figure(figsize=size)
     heatmap = sns.heatmap(correlation, annot=True, linewidths=0, vmin=-1, cmap="RdBu_r")
     
+
 def specific_correlation(feature1,feature2): 
-    #Visualize the co-relation between quantitative variables
-    #Create a new dataframe containing only two columns to visualize their co-relations
     feature1_feature2 = data[[feature1, feature2]]
-    #Initialize a joint-grid with the dataframe, using seaborn library
     gridA = sns.JointGrid(x=feature2, y=feature1, data=feature1_feature2, size=6)
-    #Draws a regression plot in the grid 
     gridA = gridA.plot_joint(sns.regplot, scatter_kws={"s": 10})
-    #Draws a distribution plot in the same grid
     gridA = gridA.plot_marginals(sns.distplot)
+
 
 def specific_correlation_quantitative(feature1,feature2,title,size):
     fig, axs = plt.subplots(ncols=1,figsize=size)
@@ -310,7 +262,6 @@ def specific_correlation_quantitative(feature1,feature2,title,size):
     plt.gcf().clear()
 
 
-#PCA
 def value_repetition(df):
     i = 0
     counter = len(df)
@@ -322,8 +273,7 @@ def value_repetition(df):
             data_count[str(df[i])] = 1  
         i += 1
     return(data_count)
-#function that counts how often a same value is repeated in the dataset
-#this function will return the result as a dictionnary giving for each distinct value in the dataset the number of times it appears
+
 
 def data_distribution(rows):
     parameter = []
@@ -345,6 +295,7 @@ def data_distribution_plot(output_dictionnary, x_label, y_label ,graph_title):
     plt.ylabel(y_label)
     plt.title(graph_title)
     plt.show()
+
 
 def PCA_generator(df):
     i = 0
@@ -391,12 +342,14 @@ def pca_components(df, nb_components):
     X_projected = pca.transform(X_scaled)  
     pcs = pca.components_
 
+
 def qualitative_representation(feature,x_axis):
     df[feature].value_counts(normalize=True).plot(kind='pie')
     plt.axis(x_axis) 
     plt.show() 
     df[feature].value_counts(normalize=True).plot(kind='bar')
     plt.show()
+
 
 def quantitative_representation(feature1,feature2,bucket_value,sample_size):
     bin_numebr = 1 + math.log2(sample_size)
@@ -431,6 +384,7 @@ def create_set(df,target):
     X_test = scaler.transform(X_test)
     X_val = scaler.transform(X_val)
     
+
 def remove_colinar_features(target,local_threshold,df):
     corr_matrix = df.corr().abs()
     i = 0
@@ -441,12 +395,10 @@ def remove_colinar_features(target,local_threshold,df):
             features_to_keep.append(df.columns[i])
         i += 1
     print(features_to_keep)
-
     label = df[target]
     i = features_to_keep.index(target)
     del features_to_keep[i]
     print(features_to_keep)
-
     i = 0
     while i < len(features_to_keep):
         corr_matrix = df[features_to_keep].corr().abs()
@@ -464,24 +416,18 @@ def remove_colinar_features(target,local_threshold,df):
     return(features_to_keep)
 
 
-    
-
 def PCA_generator(df,threshold):
     i = 0
     j = 0
     size = len(df.columns)
     X = df.values
     X_std = StandardScaler().fit_transform(X)
-# Clculating Eigenvectors and eigenvalues of Cov matirx
     mean_vec = np.mean(X_std, axis=0)
     cov_mat = (X_std - mean_vec).T.dot((X_std - mean_vec)) / (X_std.shape[0]-1)
     cov_mat = np.cov(X_std.T)
     eig_vals, eig_vecs = np.linalg.eig(cov_mat)
-# Make a list of (eigenvalue, eigenvector) tuples
     eig_pairs = [(np.abs(eig_vals[i]), eig_vecs[:,i]) for i in range(len(eig_vals))]
- #  Sort the (eigenvalue, eigenvector) tuples from high to low
     eig_pairs.sort(key=lambda x: x[0], reverse=True)
-# Visually confirm that the list is correctly sorted by decreasing eigenvalues
     for i in eig_pairs:
         print(i[0])
     tot = sum(eig_vals)
@@ -520,6 +466,7 @@ def pca_components(df, nb_components):
     X_projected = pca.transform(X_scaled)  
     pcs = pca.components_
     
+
 def label_encoding(features_list):
     if df[features_list[i]].dtypes == 'object':
         feature = list(set(df[features_list[i]].values.tolist()))
@@ -530,8 +477,10 @@ def label_encoding(features_list):
     else:
         pass
     
+
 def hasNumbers(inputString):
     return any(char.isdigit() for char in inputString)
+
 
 def remove_name(nlp,df):
     columns_to_remove =  []
@@ -546,6 +495,7 @@ def remove_name(nlp,df):
                     if token.pos_ == 'PROPN' and token.tag_ == 'NNP' and  token.dep_ == 'compound':
                         columns_to_remove.append(column)
     return(list(set(columns_to_remove)))
+
 
 def create_optimal_dataset(list_features,principalDf1,label):
     my_data = {}
